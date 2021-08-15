96f1c92207b580eef36f11c704f77a1e279263ce
jonasgozdecki <jonasgozdecki@hotmail.com>
2021-08-15 01:16:05 -0300

    finished program

--- src/calculator.js
+++ src/calculator.js
@@ -11,12 +11,32 @@ export default class StringCalculator {
 
   formatInput(input) {
     const delimiterRegExp = /^(\/\/.*\n)/; // excepting // and /n in the group
-    return input
+    const matches = delimiterRegExp.exec(input);
+    if (matches && matches.length > 0) {
+      return input.replace(delimiterRegExp, ""); //without delimiters definitions
+    }
+    return input;
   }
 
   getDelimiter(input) {
     let delimiters = [];
-    return input
+    const multDelimiterRx = /\[([^])]/g; //matches any character, including newline characters.
+    let matches = multDelimiterRx.exec(input);
+
+    const inputLimit = /.+?(?=\n)|[^0-9]/.exec(input);
+
+    if (inputLimit[0].indexOf(",") >= 3) {
+      delimiters = inputLimit[0].replace("//", "").split(",");
+    }
+
+    if (delimiters.length > 0) {
+      return new RegExp("[" + delimiters.join("") + "]");
+    }
+    matches = /^\/\/(.*)\n/.exec(input); //match a string that starts with a // and ends with \n
+    if (matches && matches[1]) {
+      return matches[1];
+    }
+    return /[\n,]/;
   }
 
   getNumbers(string, delimiter) {
@@ -28,7 +48,21 @@ export default class StringCalculator {
 
   calculateSum(numbers) {
     const negatives = [];
-    
+    const finalSum = numbers.reduce((sum, n) => {
+      if (n > 1000) {
+        return sum;
+      }
+      if (n < 0) {
+        negatives.push(n);
+        return 0;
+      }
+      return sum + n;
+    }, 0);
+    if (negatives.length > 0) {
+      throw new NegativeNumberError(
+        "Negatives not allowed: " + negatives.join(",")
+      );
+    }
     return finalSum;
   }
 }
